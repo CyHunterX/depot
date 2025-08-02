e3609e33c427c35bea76a3268c15034f25c96220
cyhunterx <cyhunterx@gmail.com>
2025-08-02 17:59:59 -0400

    Seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+
+product = Product.create(title: 'Rails Scales!',
+  description:
+    %(<p>
+  )
